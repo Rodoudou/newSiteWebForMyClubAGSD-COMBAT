6a5091ba26db01db0232bfda18983c0ffa4ab971
Redouane <Redouane@iMac-de-User.local>
2020-09-29 00:59:25 +0200

    add banner bcg size : cover

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Banner from "../../assets/img/1.jpg"
+
 
 function Nav() {
   return (
     <nav>
+
       <h2>AGSD COMBAT Saint-Denis (93)</h2>
       <p>
         Sports de combats pour les adultes et les enfants à partir de 3 ans sur
