8f4a3b925e334f964041bf57afea46a237320d19
Redouane <Redouane@iMac-de-User.local>
2020-10-06 14:06:26 +0200

    supprimer Switch et mettres Routes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes  } from "react-router-dom";
 import Nav from "./components/nav";
-import Menu from './components/menu'
-import Home from './containers/home';
-import Contact from './containers/contact';
-import Footer from './components/footer'
+import Menu from "./components/menu";
+import Home from "./containers/home";
+import Contact from "./containers/contact";
+import Footer from "./components/footer";
 import Inscription from "./containers/inscription";
 import Horaires from "./containers/horaires";
 import Tarifs from "./containers/tarifs";
 import CertificatMedical from "./containers/certificat-medical";
-
 
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Nav/>
-      <Menu/>
-      <Switch>
+      <Nav />
+      <Menu />
+      <Routes>
         <Route exact path="/">
-          <Home/>
+          <Home />
         </Route>
         <Route path="/contact">
-          <Contact/>
+          <Contact />
         </Route>
         <Route path="/inscription">
-          <Inscription/>
+          <Inscription />
         </Route>
         <Route path="/horaires">
-          <Horaires/>
+          <Horaires />
         </Route>
         <Route path="/tarifs">
-          <Tarifs/>
+          <Tarifs />
         </Route>
         <Route path="/certificat-medical">
-          <CertificatMedical/>
+          <CertificatMedical />
         </Route>
-
-      </Switch>
-      <Footer/>
+      </Routes>
+      <Footer />
     </Router>
   );
 }
